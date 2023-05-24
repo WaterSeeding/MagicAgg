6a1a4b1835fec02a3a41864d35d056e30cb54122
JYuan <13826506407@139.com>
2023-05-25 00:36:17 +0800

    设置魔法蛋成功

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,159 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { SelectiveBloom } from "./js/SelectiveBloom";
+import { DatGUI } from "./js/DatGUI";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [isInitScene, setIsInitScene] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isInitScene) {
+      setIsInitScene(true);
+      initScene();
+    }
+  }, []);
+
+  const initScene = async () => {
+    const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("canvas.webgl")!,
+      antialias: true,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0x000000, 0.0);
+    renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+      40,
+      window.innerWidth / window.innerHeight,
+      1,
+      200
+    );
+    camera.position.set(0, 5, 10);
+    camera.lookAt(0, 0, 0);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0.5, 0);
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 6;
+
+    let selectiveBloom = new SelectiveBloom(scene, camera, renderer);
+    new DatGUI(renderer, selectiveBloom);
+
+    let light = new THREE.DirectionalLight();
+    light.position.setScalar(1);
+    scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
+
+    let { model, mixer }: any = await addModel(scene);
+    let { ring }: any = await addRing(scene);
+
+    let clock = new THREE.Clock();
+
+    window.addEventListener("resize", onResize);
+    renderer.setAnimationLoop(() => {
+      let delta = clock.getDelta();
+      if (mixer) mixer.update(delta);
+
+      if (model) {
+        model.material.color.set(0x000000);
+        ring.material.color.set(0x000000);
+      }
+      renderer.setClearColor(0x000000);
+      selectiveBloom.bloomComposer.render();
+      if (model) {
+        model.material.color.copy(model.userData.color);
+        ring.material.color.copy(ring.userData.color);
+      }
+      renderer.setClearColor(0x1d1d1d);
+      selectiveBloom.finalComposer.render();
+
+      // renderer.render(scene, camera);
+      controls.update();
+    });
+
+    function onResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      selectiveBloom.bloomComposer.setSize(
+        window.innerWidth,
+        window.innerHeight
+      );
+      selectiveBloom.finalComposer.setSize(
+        window.innerWidth,
+        window.innerHeight
+      );
+    }
+  };
+
+  const addModel = (scene: THREE.Scene) => {
+    return new Promise((resolve, reject) => {
+      let model: THREE.Object3D | any;
+      let mixer: THREE.AnimationAction | any;
+      let loader = new GLTFLoader();
+      loader.load("https://cywarr.github.io/small-shop/Egg.glb", (gltf) => {
+        model = gltf.scene.getObjectByName("Egg_with_Animation");
+        model!.userData = {
+          // @ts-ignore;
+          color: new THREE.Color().copy(model.material.color),
+        };
+
+        mixer = new THREE.AnimationMixer(gltf.scene);
+        mixer.clipAction(gltf.animations[0]).play();
+
+        model.position.setY(1.0);
+        scene.add(model!);
+
+        resolve({
+          model,
+          mixer,
+        });
+      });
+    });
+  };
+
+  const addRing = (scene: THREE.Scene) => {
+    return new Promise((resolve, reject) => {
+      const textureloader = new THREE.TextureLoader();
+      textureloader.load(
+        "./model/textures/Material.001_diffuse.png",
+        function (texture) {
+          let ring: THREE.Object3D | any;
+          let loader = new GLTFLoader();
+          loader.load("./model/scene.gltf", (gltf) => {
+            ring = gltf.scene.getObjectByName("Plane_0");
+            ring.material = new THREE.MeshStandardMaterial({
+              map: texture,
+              transparent: true
+            });
+            ring.userData = {
+              // @ts-ignore;
+              color: new THREE.Color().copy(ring.material.color),
+            };
+            ring.rotateX(-Math.PI / 2);
+            ring.scale.set(2, 2, 2);
+            scene.add(ring);
+
+            resolve({
+              ring,
+            });
+          });
+        }
+      );
+    });
+  };
+
   return (
     <div className="App">
-      测试效果
+      <canvas
+        className="webgl"
+        style={{ width: "100%", height: "100%" }}
+      ></canvas>
     </div>
   );
 }
-
-export default App;
