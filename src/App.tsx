fb8f7b2482965e0d8503d7588cca849e6ba1472d
JYuan <13826506407@139.com>
2023-08-13 15:59:25 +0800

    feat: 添加天空盒效果和音频

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
@@ -25,12 +25,16 @@ const getCubeMapTexture = (renderer: THREE.WebGLRenderer, path: string) => {
 };
 
 export default function App() {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isInitScene, setIsInitScene] = useState<boolean>(false);
+  const [isAudioPlay, setIsAudioPlay] = useState<boolean>(false);
 
   useEffect(() => {
     if (!isInitScene) {
       setIsInitScene(true);
       initScene();
+
+      let audioPlayer = audioRef.current as HTMLAudioElement;
     }
   }, []);
 
@@ -52,12 +56,13 @@ export default function App() {
       1,
       200
     );
-    camera.position.set(0, 2, 10);
+    camera.position.set(0, 4, 20);
     camera.lookAt(0, 0, 0);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 0, 0);
     controls.autoRotate = true;
+    controls.enableZoom = false;
     controls.autoRotateSpeed = 0.5;
 
     let envMap = (await getCubeMapTexture(
@@ -69,6 +74,7 @@ export default function App() {
     light.position.setScalar(1);
     scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
 
+    let { world }: any = await addWorld(scene);
     let { model, mixer }: any = await addModel(scene);
     let { ring }: any = await addRing(scene);
 
@@ -90,6 +96,7 @@ export default function App() {
       if (mixer) mixer.update(delta);
 
       if (model) {
+        world.material.color.set(0x000000);
         model.material.color.set(0x000000);
         ring.material.color.set(0x000000);
         scene.background = null;
@@ -97,6 +104,7 @@ export default function App() {
       renderer.setClearColor(0x000000);
       selectiveBloom.bloomComposer.render();
       if (model) {
+        world.material.color.copy(world.userData.color);
         model.material.color.copy(model.userData.color);
         ring.material.color.copy(ring.userData.color);
       }
@@ -152,6 +160,31 @@ export default function App() {
     });
   };
 
+  const addWorld = (scene: THREE.Scene) => {
+    return new Promise((resolve, reject) => {
+      let geometry = new THREE.SphereGeometry(15, 64, 32);
+      geometry.scale(-1, 1, 1);
+
+      let material = new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load(
+          "./hdr/fantasy_lands_galaxy_worldsuper_moon_all_starre.jpg"
+        ),
+        depthTest: false,
+      });
+
+      let world = new THREE.Mesh(geometry, material);
+
+      world!.userData = {
+        // @ts-ignore;
+        color: new THREE.Color().copy(world.material.color),
+      };
+      scene.add(world);
+      resolve({
+        world,
+      });
+    });
+  };
+
   const addRing = (scene: THREE.Scene) => {
     return new Promise((resolve, reject) => {
       const textureloader = new THREE.TextureLoader();
@@ -184,6 +217,16 @@ export default function App() {
     });
   };
 
+  const setAudioPlay = (value: boolean) => {
+    let audioPlayer = audioRef.current as HTMLAudioElement;
+    if (value) {
+      audioPlayer.play();
+    } else {
+      audioPlayer.pause();
+    }
+    setIsAudioPlay(value);
+  };
+
   return (
     <div className="App">
       <div className="content">
@@ -195,6 +238,13 @@ export default function App() {
           Made in Three.js
         </a>
       </div>
+      <div
+        className={`audioIcon ${isAudioPlay && "active"}`}
+        onClick={() => setAudioPlay(!isAudioPlay)}
+      ></div>
+      <audio ref={audioRef} loop autoPlay={true}>
+        <source src={"./audio/WINDY_HILL.mp3"} />
+      </audio>
       <canvas
         className="webgl"
         style={{ width: "100%", height: "100%" }}
